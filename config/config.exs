29e4a118f2e213154f35f562c9f2d40137026a41
Fatigue <fatigue.dev@gmail.com>
2022-06-14 18:21:51 +1000

    Maybe

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,8 @@ import Config
 
 config :venyou, Venyou.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
+  url: "postgresql://7bd9ead8-400e-458a-a91d-be8b55c200ee-user:pw-35bcc6bd-baf6-4c65-980f-2202bcfee000@postgres-free-tier-v2020.gigalixir.com:5432/7bd9ead8-400e-458a-a91d-be8b55c200ee",
+  database: "7bd9ead8-400e-458a-a91d-be8b55c200ee",
   ssl: true,
   pool_size: 2
 
