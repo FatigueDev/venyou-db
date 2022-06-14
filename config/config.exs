61905df6c76ae89446a4295f040ffdddd237dc90
Fatigue <fatigue.dev@gmail.com>
2022-06-14 18:20:05 +1000

    Sigh

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,10 @@
 import Config
 
 config :venyou, Venyou.Repo,
-  database: "7bd9ead8-400e-458a-a91d-be8b55c200ee",
-  username: "7bd9ead8-400e-458a-a91d-be8b55c200ee-user",
-  password: "pw-35bcc6bd-baf6-4c65-980f-2202bcfee000",
-  hostname: "postgres-free-tier-v2020.gigalixir.com",
-  port: "5432"
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"},
+  database: "",
+  ssl: true,
+  pool_size: 2
 
 config :venyou, ecto_repos: [Venyou.Repo]
