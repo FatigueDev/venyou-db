e886bd15fa018eb4c7f6c4eb2d30d954dc05a8fe
Fatigue <fatigue.dev@gmail.com>
2022-06-16 13:28:58 +1000

    Test

--- config/prod.exs
+++ config/prod.exs
@@ -1,10 +1,5 @@
-import Config
+# import Config
 
-config :venyou, Venyou.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
-  ssl: true,
-  pool_size: 2
 
-config :venyou, ecto_repos: [Venyou.Repo]
+
+# config :venyou, ecto_repos: [Venyou.Repo]
