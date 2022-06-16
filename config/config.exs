3c22c893ad6640b7db62d2d61db979f9c4f2257f
Fatigue <fatigue.dev@gmail.com>
2022-06-16 13:39:44 +1000

    Updated config

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,16 @@
 import Config
 
-# config :venyou, Venyou.Repo,
-#   database: "venyou_repo",
-#   username: "postgres",
-#   password: "Momomo12",
-#   hostname: "localhost"
-
 config :venyou, Venyou.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  pool_size: 2
+  database: "venyou_repo",
+  username: "postgres",
+  password: "Momomo12",
+  hostname: "localhost"
+
+# config :venyou, Venyou.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   url: System.get_env("DATABASE_URL"),
+#   ssl: true,
+#   pool_size: 2
 
 # config :venyou, Venyou.Repo,
 #   adapter: Ecto.Adapters.Postgres,
