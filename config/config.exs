c36246a067825d2990f00ca37db8fa436a1adcc3
Fatigue <fatigue.dev@gmail.com>
2022-06-16 14:26:32 +1000

    Config.

--- config/config.exs
+++ config/config.exs
@@ -1,30 +1,10 @@
 import Config
 
 config :venyou, Venyou.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    database: "",
-    ssl: true,
-    url: System.get_env("DATABASE_URL"),
-    pool_size: String.to_integer(System.get_env("POOL_SIZE"))
-
-config :venyou, Venyou.Plug,
-  load_from_system_env: true,
-  http: [port: {:system, "PORT"}],
-  server: true,
-  secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443]
-
-# config :venyou, Venyou.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: System.get_env("DATABASE_URL"),
-#   ssl: true,
-#   pool_size: 2
-
-# config :venyou, Venyou.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: {:system, "DATABASE_URL"},
-#   database: "",
-#   ssl: true,
-#   pool_size: 2
+  database: "182f6029-be42-4794-a307-bb3774a9f975",
+  username: "182f6029-be42-4794-a307-bb3774a9f975-user",
+  password: "pw-102e0183-3292-4548-aedb-e90dd1b5ce7b",
+  url: "postgresql://182f6029-be42-4794-a307-bb3774a9f975-user:pw-102e0183-3292-4548-aedb-e90dd1b5ce7b@postgres-free-tier-v2020.gigalixir.com:5432/182f6029-be42-4794-a307-bb3774a9f975"
+  hostname: "https://venyou.gigalixirapp.com/"
 
 config :venyou, ecto_repos: [Venyou.Repo]
