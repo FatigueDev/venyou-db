8f7a958bea06582d2f90bfdf4b168b69474254f0
Fatigue <fatigue.dev@gmail.com>
2022-06-16 13:20:40 +1000

    Config

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,9 @@
+import Config
+
 config :venyou, Venyou.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   ssl: true,
   pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
+config :venyou, ecto_repos: [Venyou.Repo]
