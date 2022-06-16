d0a96542805cf52c4196f94b63348bab33ccb1e8
Fatigue <fatigue.dev@gmail.com>
2022-06-16 13:31:03 +1000

    Test 1

--- config/config.exs
+++ config/config.exs
@@ -8,9 +8,15 @@ import Config
 
 config :venyou, Venyou.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL"},
-  database: "",
+  url: System.get_env("DATABASE_URL"),
   ssl: true,
-  pool_size: 2
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
+# config :venyou, Venyou.Repo,
+#   adapter: Ecto.Adapters.Postgres,
+#   url: {:system, "DATABASE_URL"},
+#   database: "",
+#   ssl: true,
+#   pool_size: 2
 
 config :venyou, ecto_repos: [Venyou.Repo]
