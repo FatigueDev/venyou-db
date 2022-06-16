9d010cb1cdba384c5386a1e52b7420c62770d65d
Fatigue <fatigue.dev@gmail.com>
2022-06-16 14:23:55 +1000

    Plug?

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,13 @@ config :venyou, Venyou.Repo,
     url: System.get_env("DATABASE_URL"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE"))
 
+config :venyou, Venyou.Plug,
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443]
+
 # config :venyou, Venyou.Repo,
 #   adapter: Ecto.Adapters.Postgres,
 #   url: System.get_env("DATABASE_URL"),
